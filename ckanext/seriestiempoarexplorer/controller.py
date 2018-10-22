1eed08a53a8f71d53fdc0db8fff59d74386e9fb5
Christian Digiorno <cdigiorno@devartis.com>
2018-10-22 11:05:40 -0300

    Utilizo la URI que existe en el archivo de configuración del nodo

--- ckanext/seriestiempoarexplorer/controller.py
+++ ckanext/seriestiempoarexplorer/controller.py
@@ -1,5 +1,6 @@
 from logging import getLogger
 
+from pylons.config import config
 import ckan.lib.base as base
 
 logger = getLogger(__name__)
@@ -7,4 +8,5 @@ logger = getLogger(__name__)
 
 class TSArController(base.BaseController):
     def series_tiempo(self):
-        return base.render('seriestiempoarexplorer/series_explorer.html', extra_vars={})
+        return base.render('seriestiempoarexplorer/series_explorer.html',
+                           extra_vars={'series_api_uri': config.get('seriesapi.site_uri')})
